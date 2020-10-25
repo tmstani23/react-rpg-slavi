9d7dd1758811b99bda0a845692356da0d1bb8964
tmstani23 <tmstani23@gmail.com>
2020-10-25 17:40:28 -0500

    rpg 01
    Created sprite styles and added sprite to the background image for the sprite component.  Added sprite component to main app.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import Sprite from './components/sprite'
+import Sprite from './components/sprite';
+import maleSprite1 from './resources/m1.png';
 
 function App() {
+  //console.log(maleSprite01)
+  
   return (
     <div className = "zone-container">
-      <Sprite />
+      <Sprite image={maleSprite1} data={{
+        x: 0,
+        y: 0,
+        h: 32,
+        w: 32
+      }}/>
     </div>
   );
 }
--- client/src/components/sprite/index.js
+++ client/src/components/sprite/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
-const Sprite = () => {
-    return (<div
+import '../../resources/styles.css';
+import maleSprite01 from '../../resources/m1.png';
+
+const Sprite = (image, data) => {
+    
+    const {x, y, h , w} = data;
+    const spriteImage = image.image
+
+    console.log(image)
+    
+    return (
+        <div
             style={{
                 display: "inline-block",
-                height:"32px",
-                width:"32px",
-                backgroundImage: "url(/resources/m1.png)",
+                height:`32px`,
+                width:`32px`,
+                backgroundImage: `url(${spriteImage})`,
                 backgroundRepeat: "no-repeat",
                 // starting point - top left sprite in the sprite sheet
                 backgroundPosition: "0px 0px"
